0892369d07144f1a54de1bb5829357ae1ff89853
zakeramani <zaker.amani@gmail.com>
2023-12-06 16:03:15 +0330

    this is first commit for learning github and eclipse

---
+++ src/firstproject/Test1.java
@@ -0,0 +1,11 @@
+package firstproject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first program in github");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module firstproject {
+}
