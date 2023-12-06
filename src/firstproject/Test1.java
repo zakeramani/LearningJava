259af579cea7b0198b45ab9b079625ac3386e214
zakeramani <zaker.amani@gmail.com>
2023-12-06 18:57:22 +0330

    این برای تست دوم است

--- src/firstproject/Test1.java
+++ src/firstproject/Test1.java
@@ -9,6 +9,11 @@ public class Test1 {
 		System.out.println("this is my first program in github");
 		System.out.println("this is my first program in github");
 		System.out.println("this is my first program in github");
+		System.out.println("this is my first program in github");
+		System.out.println("this is my first program in github");
+		System.out.println("this is my first program in github");
+		System.out.println("this is my first program in github");
+		System.out.println("this is my first program in github");
 
 
 
