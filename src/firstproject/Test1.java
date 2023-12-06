bff2885175bc17d6b885fd27110a38dc734b452f
zakeramani <zaker.amani@gmail.com>
2023-12-06 19:08:56 +0330

    conflit test

--- src/firstproject/Test1.java
+++ src/firstproject/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my first program in github");
+		System.out.println("this is my first program in github no");
 		System.out.println("this is my first program in github");
 		System.out.println("this is my first program in github");
 		System.out.println("this is my first program in github");
